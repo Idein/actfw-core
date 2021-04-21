a45b49c4061c1d73791d73f8f8d32b6319989087
keno <keno.ss57@gmail.com>
2021-04-21 10:54:07 +0900

    refactor: Use Queue(1) and simplify code

--- actfw_core/util/pad.py
+++ actfw_core/util/pad.py
@@ -4,13 +4,6 @@ from abc import ABC, abstractmethod
 from queue import Empty, Queue
 from typing import Generic, Optional, Tuple, TypeVar
 
-_pyversion = sys.version_info.major * 10000 + sys.version_info.minor * 100
-if _pyversion >= 30700:  # >= "3.7"
-    _SimpleQueue = queue.SimpleQueue
-else:
-    _SimpleQueue = queue.Queue
-
-
 T = TypeVar("T")
 
 
@@ -66,10 +59,10 @@ class _PadBlocking(_PadBase[T]):
 
 
 class _PadDiscardingOld(Generic[T]):
-    _queue: "_SimpleQueue[T]"
+    _queue: "Queue[T]"
 
     def __init__(self) -> None:
-        self._queue = _SimpleQueue()
+        self._queue = Queue(1)
 
     def empty(self) -> bool:
         return self._queue.empty()
@@ -82,15 +75,11 @@ class _PadDiscardingOld(Generic[T]):
     ) -> None:
         # First, make the queue empty.
         # Notice that only the owner of `_PadIn` can `put()`, but the owner can call `put()` cuncurrently.
-        i = self._queue.qsize()
-        while i != 0:
-            i -= 1
-            try:
-                self.get(block=False)
-            except Empty:
-                pass
-        # Note that the arguments `block` and `timeout` are meaningless, because of the property of `SimpleQueue`.
-        # (Except the case python version < 3.7.)
+        try:
+            self.get(block=False)
+        except Empty:
+            pass
+        # Then put.
         self._queue.put(item, block=block, timeout=timeout)
 
     def get(
