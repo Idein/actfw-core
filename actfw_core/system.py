a6982aa5156a8d1319d55e9e360b481f85aaeb5b
eguchi1904 <eguchi.1904@gmail.com>
2022-11-01 17:51:26 +0900

    add DeviceSupply

--- actfw_core/system.py
+++ actfw_core/system.py
@@ -1,5 +1,7 @@
 import os
-from typing import Optional
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Any, Dict, List, Optional
 
 
 class EnvironmentVariableNotSet(Exception):
@@ -97,3 +99,61 @@ def get_actcast_agent_simulator() -> Optional[str]:
 def get_actcast_firmware_type() -> str:
     "Firmware type of the host. Since ACTCAST_PROTOCOL_VERSION 1.3.0."
     return _get_env_str("ACTCAST_FIRMWARE_TYPE")
+
+
+@dataclass
+class DeviceNode:
+    path: Path
+    label: Optional[str] = None
+
+    @staticmethod
+    def from_json(json: Dict[str, Any]) -> "DeviceNode":
+        path = json.get("path", None)
+        if path is None:
+            raise ValueError("must have path property")
+
+        if type(path) is not str:
+            raise ValueError("path must be string")
+
+        path = Path(path)
+
+        label = json.get("label", None)
+        if label is None:
+            return DeviceNode(path=path, label=None)
+        else:
+            if type(label) is not str:
+                raise ValueError("label must be string")
+            return DeviceNode(path=path, label=label)
+
+
+@dataclass
+class DeviceInfo:
+    type: str
+    nodes: List[DeviceNode]
+
+    @staticmethod
+    def from_json(json: Dict[str, Any]) -> "DeviceInfo":
+        if "type" not in json.keys():
+            raise ValueError("must have type property")
+        if "nodes" not in json.keys():
+            raise ValueError("must have nodes property")
+
+        device_type = json["type"]
+        if type(device_type) is not str:
+            raise ValueError("type must be string")
+
+        nodes = [DeviceNode.from_json(node) for node in json["nodes"]]
+        return DeviceInfo(type=device_type, nodes=nodes)
+
+
+@dataclass
+class DeviceSupply:
+    devices: List[DeviceInfo]
+
+    @staticmethod
+    def from_json(json: Dict[str, Any]) -> "DeviceSupply":
+        if "devices" not in json.keys():
+            raise ValueError("must have devices property")
+
+        devices = [DeviceInfo.from_json(d) for d in json["devices"]]
+        return DeviceSupply(devices=devices)
